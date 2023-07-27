a9603c6c87e5687a5690fc25c89d3eb3f9b0baba
bober2005 <127338926+bober2005@users.noreply.github.com>
2023-07-27 13:05:12 +0300

    3

---
+++ js/task-09.js
@@ -0,0 +1,12 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const colorSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
